b06577606e4f6d2e501bfc52c64c8a78c96d3941
Nathan Griffin <nathan.griffin@ubnt.com>
2015-03-10 17:49:07 -0400

    changes to build into community-themes folder

--- gulpfile.js
+++ gulpfile.js
@@ -28,27 +28,26 @@ var fontFilter = $.filter(['**/**/*.ttf', '*.ttf', '**/**/*.woff', '*.woff']);
 //
 
 var outputs = {
-    base: 'dist',
-    dev: 'dist/dev',
-    prod: 'dist/prod',
-    js: '/js',
-    css: '/css',
-    img: '/img'
+    dev: '../community-themes/UbntUI/dist/dev/js',
+    prod: '../community-themes/UbntUI/dist/prod/js',
+    index: {
+        toDirectory: '../community-themes/UbntUI/applications/ubnt-forums/views/u/',
+        fromDirectory: '../../../dist',
+        filename: 'index.php'
+    } 
 };
 
 
 var sources = {
 	js: ['app/**/*.js'],
     dependencyJs: [
-        outputs.dev + outputs.js + '/areas/**/config/*.js',
-        outputs.dev + outputs.js + '/areas/**/*.js',
-        outputs.dev + outputs.js + '/shared/**/*.module.js',
-        outputs.dev + outputs.js + '/shared/**/*.js',
-        outputs.dev + outputs.js + '/templates/**/*.js',
-        outputs.dev + outputs.js + '/app.init.js'
+        outputs.dev + '/areas/**/config/*.js',
+        outputs.dev + '/areas/**/*.js',
+        outputs.dev + '/shared/**/*.module.js',
+        outputs.dev + '/shared/**/*.js',
+        outputs.dev + '/templates/**/*.js',
+        outputs.dev + '/app.init.js'
     ],
-	fonts: ['Content/**/*.ttf'],
-	img: ['content/**/*.png'],
 	sass: ['app/styles/*.scss'],
 	partials: [
 		'app/areas/**/*.html',
@@ -64,13 +63,6 @@ var sources = {
 //
 //
 
-gulp.task('index', function () {
-    return gulp.src('app/index.html')
-        .pipe($.rename("index.html"))
-        .pipe(gulp.dest(outputs.dev))
-        .pipe(gulp.dest(outputs.prod));
-});
-
 gulp.task('clean', function(){
     del([outputs.base]);
 });
@@ -83,7 +75,7 @@ gulp.task('clean', function(){
 
 gulp.task('scripts', function() {
   return gulp.src(sources.js)
-    .pipe(gulp.dest(outputs.dev + outputs.js));
+    .pipe(gulp.dest(outputs.dev));
 });
 
 gulp.task('prod-scripts', function(){
@@ -91,41 +83,20 @@ gulp.task('prod-scripts', function(){
         .pipe(jsFilter)
         .pipe($.concat('app.min.js'))
         .pipe($.uglify())
-        .pipe(gulp.dest(outputs.prod + outputs.js))
+        .pipe(gulp.dest(outputs.prod))
         .pipe(jsFilter.restore());
-});
-
-gulp.task('styles', function() {
-    return gulp.src(sources.sass)
-        .pipe($.compass({ sass: './app/styles/', css: outputs.dev + outputs.css }))
-        .pipe($.concat('styles.min.css'));
-});
-
-gulp.task('prod-styles', function(){
-    return gulp.src(bowerFiles.concat([outputs.dev + outputs.css + '/*.css']))
-        .pipe(cssFilter)
-        .pipe($.minifyCss())
-        .pipe($.rename('app.min.css'))
-        .pipe(gulp.dest(outputs.prod + outputs.css))
-        .pipe(cssFilter.restore());
 });
 
 gulp.task('partials', function() {
     var partialGlob = gulp.src(sources.partials)
         .pipe($.ngHtml2js({ moduleName: "community-templates" }))
-        .pipe(gulp.dest(outputs.dev + outputs.js + '/templates'));
-});
-
-gulp.task('img', function() {
-    return gulp.src(sources.img)
-        .pipe(gulp.dest(outputs.dev + outputs.img))
-        .pipe(gulp.dest(outputs.prod + outputs.img));
+        .pipe(gulp.dest(outputs.dev + '/templates'));
 });
 
 gulp.task('bower', function(){
     return gulp.src(bowerFiles)
         .pipe(jsFilter)
-        .pipe(gulp.dest(outputs.dev + outputs.js + '/vendor'))
+        .pipe(gulp.dest(outputs.dev + '/vendor'))
         .pipe(jsFilter.restore());
 });
 
@@ -137,10 +108,11 @@ gulp.task('bower', function(){
 
 
 gulp.task('inject', ['build'], function () {
-   	var devLayoutPage = gulp.src('./dist/dev/index.html');
-    var devAppFiles =   sources.dependencyJs.concat(['dist/dev/css/**/*.css']);
+    var index = outputs.index;
+   	var devLayoutPage = gulp.src(index.toDirectory + index.filename);
+    var devAppFiles =   sources.dependencyJs;
 
-    devLayoutPage = devLayoutPage
+    return devLayoutPage
         .pipe($.inject(gulp.src(bowerFiles, { read: false }), {
                 name: 'bower',
                 ignorePath: 'vendor/bower/',
@@ -150,25 +122,29 @@ gulp.task('inject', ['build'], function () {
                     var cleanFilepath = filepath.replace(/^[/\\\\]?(?:.+[/\\\\]+?)?(.+?)[/\\\\]/, '');
                     var extension = cleanFilepath.split('.').pop();
                     if (extension == 'js') {
-                        newPath = '<script src="js/vendor/' + cleanFilepath + '"></script>';
-                    } else if (extension == 'css') {
-                        newPath = '<link rel="stylesheet" href="css/vendor/' + cleanFilepath + '">';
-                    }
+                        newPath = '<script src="' + index.fromDirectory + '/dev/js/vendor/' + cleanFilepath + '"></script>';
+                    } 
                     return newPath;
                 }
             }))
-        .pipe($.inject(gulp.src(devAppFiles, { read: false }), { name: 'app',  addRootSlash: false, ignorePath: "dist/dev/"  }))
-        .pipe($.rename('index.html'))
-        .pipe(gulp.dest('./dist/dev'));
+        .pipe($.inject(gulp.src(devAppFiles, { read: false }), { 
+            name: 'app',  
+            addRootSlash: false, 
+            ignorePath: "../community-themes/UbntUI/dist/",
+            transform: function(filepath) {
+                return '<script src="' + index.fromDirectory + '/' + filepath +'"></script>';
+            }  
+        }))
+        .pipe($.rename(index.filename))
+        .pipe(gulp.dest(index.toDirectory));
 
-    var prodLayoutPage = gulp.src('./dist/prod/index.html');
-    var prodFiles = 
-    prodLayoutPage = prodLayoutPage
-        .pipe($.inject(gulp.src(['./prod/css/*.css', './prod/scripts/*.js'], { read: false }), { name: 'app',  addRootSlash: false, ignorePath: "dist/prod/"}))
-        .pipe($.rename('index.html'))
-        .pipe(gulp.dest(outputs.prod));
+    // var prodLayoutPage = gulp.src('./dist/prod/index.html');
+    // prodLayoutPage = prodLayoutPage
+    //     .pipe($.inject(gulp.src(['./prod/css/*.css', './prod/scripts/*.js'], { read: false }), { name: 'app',  addRootSlash: false, ignorePath: "dist/prod/"}))
+    //     .pipe($.rename('index.html'))
+    //     .pipe(gulp.dest(outputs.prod));
 
-    return merge(devLayoutPage, prodLayoutPage);
+    // return merge(devLayoutPage, prodLayoutPage);
 });
 
 
@@ -180,7 +156,6 @@ gulp.task('inject', ['build'], function () {
 
 gulp.task('watch', function () {
 	gulp.watch(sources.js, ['scripts']);
-	gulp.watch(sources.sass, ['sass']);
 	gulp.watch(sources.partials, ['partials']);
 	gulp.watch(sources.img, ['img']);
 });
@@ -191,28 +166,19 @@ gulp.task('watch', function () {
 //
 //
 
-gulp.task('default', ['index', 'build', 'inject', 'watch']);
+gulp.task('default', ['dev', 'inject', 'watch']);
 
-gulp.task('dev', ['scripts', 'styles', 'partials', 'img']);
+gulp.task('dev', ['scripts', 'partials', 'bower']);
 
-gulp.task('build', ['dev', 'bower'], function(){
+gulp.task('build', ['dev'], function(){
     //build prod from dev build
     var jsFilter = $.filter(['**/*.js', '*.js']);
-    var cssFilter = $.filter(['**/*.css', '*.css']);
-    // var imgFilter = $.filter(['/**/*.png', '*.png', '**/*.gif']);
-    // var sassFilter = $.filter(['/**/*.scss', '*.scss']);
-    // var fontFilter = $.filter(['**/**/*.ttf', '*.ttf', '**/**/*.woff', '*.woff']);
-
+   
     var appFiles = bowerFiles.concat(sources.dependencyJs, ['dist/dev/css/*.css'])
     return gulp.src(appFiles)
         .pipe(jsFilter)
         .pipe($.concat('app.min.js'))
         .pipe($.uglify())
         .pipe(gulp.dest(outputs.prod + outputs.js))
-        .pipe(jsFilter.restore())
-        .pipe(cssFilter)
-        .pipe($.minifyCss())
-        .pipe($.rename('app.min.css'))
-        .pipe(gulp.dest(outputs.prod + outputs.css))
-        .pipe(cssFilter.restore());
+        .pipe(jsFilter.restore());
 });
