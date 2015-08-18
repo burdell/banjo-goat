6161863b7ce7c45acec37b3423587c2de83f2d79
Nathan Griffin <nathangriffin@Nathans-MacBook-Pro.local>
2015-08-18 15:58:10 -0400

    beginning markdown editor

--- app/shared/directives/texteditor/texteditor.js
+++ app/shared/directives/texteditor/texteditor.js
@@ -3,57 +3,61 @@
 	
 	function communityTextEditor($timeout, routingService) {
 		var link = function(scope, element, attrs, ngModel) {
-			$timeout(function(){
-				var textElement = $(element).find('#' + scope.texteditor.editorId);
-				var editorInstance = null;
+			// $timeout(function(){
+			// 	var textElement = $(element).find('#' + scope.texteditor.editorId);
+			// 	var editorInstance = null;
 
-				ngModel.$render = function(){
-					if (!editorInstance) {
-						editorInstance = tinymce.get(scope.texteditor.editorId);
-					}
+			// 	ngModel.$render = function(){
+			// 		if (!editorInstance) {
+			// 			editorInstance = tinymce.get(scope.texteditor.editorId);
+			// 		}
 
-					editorInstance.setContent(ngModel.$viewValue || '');
-				};
+			// 		editorInstance.setContent(ngModel.$viewValue || '');
+			// 	};
 				
-				var currentArea = routingService.getCurrentArea();
+			// 	var currentArea = routingService.getCurrentArea();
 
-				tinymce.init({
-					height: scope.texteditor.height || 150,
-					elements: scope.texteditor.editorId,
-					mode: 'exact',
-					content_css : "{{GULP_BUILD_areaName}}/css/editor.css",
-					menubar: false,
-					preview_styles: false,
-					browser_spellcheck: true,
-					toolbar: scope.texteditor.minimalEditor === 'true' ? false : undefined,
-					plugins: 'placeholder',
-					setup: function(editor) {
-						function updateModel() {
-							editor.save();
+			// 	tinymce.init({
+			// 		height: scope.texteditor.height || 150,
+			// 		elements: scope.texteditor.editorId,
+			// 		mode: 'exact',
+			// 		content_css : "{{GULP_BUILD_areaName}}/css/editor.css",
+			// 		menubar: false,
+			// 		preview_styles: false,
+			// 		browser_spellcheck: true,
+			// 		toolbar: scope.texteditor.minimalEditor === 'true' ? false : undefined,
+			// 		plugins: 'placeholder',
+			// 		setup: function(editor) {
+			// 			function updateModel() {
+			// 				editor.save();
 
-							ngModel.$setViewValue(textElement.val());
-							if (!scope.$$phase) {
-	                            scope.$apply();
-	                        }
-						}
+			// 				ngModel.$setViewValue(textElement.val());
+			// 				if (!scope.$$phase) {
+	  //                           scope.$apply();
+	  //                       }
+			// 			}
 
-						editor.on('KeyUp', function(e) {
-							updateModel();
-						});
+			// 			editor.on('KeyUp', function(e) {
+			// 				updateModel();
+			// 			});
 
-						editor.on('ExecCommand', function(e) {
-							updateModel();
-						});
+			// 			editor.on('ExecCommand', function(e) {
+			// 				updateModel();
+			// 			});
 
-						editor.on('init', function(){
-							ngModel.$render();
-						});
-					},
-					init_instance_callback: function(){
-						$(element).find('.mce-path').css('visibility', 'hidden');
-					}
-				});
-			}, 0);
+			// 			editor.on('init', function(){
+			// 				ngModel.$render();
+			// 			});
+			// 		},
+			// 		init_instance_callback: function(){
+			// 			$(element).find('.mce-path').css('visibility', 'hidden');
+			// 		}
+			// 	});
+			// }, 0);
+			scope.texteditor.saveText = function(textString) {
+				debugger;
+			};
+
 		};
 
 		var controller = function($scope) {
