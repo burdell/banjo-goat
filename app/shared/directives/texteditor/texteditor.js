72ab5b3b213f6f7152f94fc6225f16272a85694c
Nathan Griffin <nathangriffin@Nathans-MacBook-Pro.local>
2015-08-19 11:26:18 -0400

    markdown editor working

--- app/shared/directives/texteditor/texteditor.js
+++ app/shared/directives/texteditor/texteditor.js
@@ -1,69 +1,22 @@
-(function(_, tinymce, $){
+(function(_, marked){
 	'use strict';
 	
 	function communityTextEditor($timeout, routingService) {
 		var link = function(scope, element, attrs, ngModel) {
-			// $timeout(function(){
-			// 	var textElement = $(element).find('#' + scope.texteditor.editorId);
-			// 	var editorInstance = null;
-
-			// 	ngModel.$render = function(){
-			// 		if (!editorInstance) {
-			// 			editorInstance = tinymce.get(scope.texteditor.editorId);
-			// 		}
-
-			// 		editorInstance.setContent(ngModel.$viewValue || '');
-			// 	};
-				
-			// 	var currentArea = routingService.getCurrentArea();
-
-			// 	tinymce.init({
-			// 		height: scope.texteditor.height || 150,
-			// 		elements: scope.texteditor.editorId,
-			// 		mode: 'exact',
-			// 		content_css : "{{GULP_BUILD_areaName}}/css/editor.css",
-			// 		menubar: false,
-			// 		preview_styles: false,
-			// 		browser_spellcheck: true,
-			// 		toolbar: scope.texteditor.minimalEditor === 'true' ? false : undefined,
-			// 		plugins: 'placeholder',
-			// 		setup: function(editor) {
-			// 			function updateModel() {
-			// 				editor.save();
-
-			// 				ngModel.$setViewValue(textElement.val());
-			// 				if (!scope.$$phase) {
-	  //                           scope.$apply();
-	  //                       }
-			// 			}
-
-			// 			editor.on('KeyUp', function(e) {
-			// 				updateModel();
-			// 			});
-
-			// 			editor.on('ExecCommand', function(e) {
-			// 				updateModel();
-			// 			});
-
-			// 			editor.on('init', function(){
-			// 				ngModel.$render();
-			// 			});
-			// 		},
-			// 		init_instance_callback: function(){
-			// 			$(element).find('.mce-path').css('visibility', 'hidden');
-			// 		}
-			// 	});
-			// }, 0);
 			scope.texteditor.saveText = function(textString) {
-				debugger;
+				var markedDownText = marked(textString, scope.texteditor.markdownOptions);
+				ngModel.$setViewValue(markedDownText);
 			};
-
 		};
 
 		var controller = function($scope) {
 			var ctrl = this;
 			_.extend(ctrl, {
-				editorId: 'community-editor-' + $scope.$id
+				editorId: 'community-editor-' + $scope.$id,
+				editorHeight: ctrl.height | '150',
+				markdownOptions: {
+					sanitize: true
+				}
 			});
 
 		};
@@ -91,4 +44,4 @@
 	angular.module('community.directives')
 		.directive('communityTextEditor', communityTextEditor);
 		
-}(window._, window.tinymce, window.jQuery));
+}(window._, window.marked));
