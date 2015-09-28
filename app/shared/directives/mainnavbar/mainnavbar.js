c6baf62879c7bb705118c3cef33456912d066e64
janzheng <janeazy@gmail.com>
2015-09-28 17:12:08 -0400

    templates, replying, badgechange

--- app/shared/directives/mainnavbar/mainnavbar.js
+++ app/shared/directives/mainnavbar/mainnavbar.js
@@ -24,7 +24,7 @@
 			}
 
 			var navMetaData = [
-				{ display: "Activity", href: hrefs.feed },
+				// { display: "Activity", href: hrefs.feed }, // 'Community should take users here'
 				{ display: "Discussions", clickFn: toggleDiscussionsMenu, dropItem: true },
 				// { display: "Resources", href: "#", dropItem: true },
 				{ display: "Q&A", href: "#"},
