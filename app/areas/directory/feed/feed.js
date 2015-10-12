e4cfdd33272620fa44e39530b7c07a793636ab65
janzheng <janeazy@gmail.com>
2015-10-12 14:39:35 -0400

    - Subscriptions page: remove subs + manage subs button - Feed: add a x to close button for stories (fpo / temp for now) - Feed: done with general styling - Template: set min-height so footer isn’t so high up - Feed: made headers and non-click icons black - Feed: tabs styling

--- app/areas/directory/feed/feed.js
+++ app/areas/directory/feed/feed.js
@@ -9,6 +9,8 @@ require('shared/services/routing.js');
 require('shared/directives/feedcontent/feedcontent.js');
 require('shared/directives/sorter/sorter.js');
 require('shared/directives/pager/pager.js');
+require('directives/classtoggle/classtoggle.js');
+
 
 var _ = require('underscore');
 var feedController = function($scope, announcementData, storyData, apiService, dataService, nodeServiceWrapper, routingService, feedFilter){
--- app/shared/directives/classtoggle/classtoggle.js
+++ app/shared/directives/classtoggle/classtoggle.js
@@ -1,35 +1,36 @@
-(function(_){
-	'use strict';
-	
-	function classToggle() {
-		var link = function(scope, element, attrs) {
-			var className = attrs.communityClassToggle;
 
-			function hasClass() {
-				return _.contains(element[0].classList, className);
+'use strict';
+
+var _ = require('underscore');
+
+function classToggle() {
+	var link = function(scope, element, attrs) {
+		var className = attrs.communityClassToggle;
+
+		function hasClass() {
+			return _.contains(element[0].classList, className);
+		}
+
+		element.on('click', function(){
+			if (hasClass()) {
+				this.classList.remove(className);
+			} else {
+				this.classList.add(className);
 			}
+		});
 
-			element.on('click', function(){
-				if (hasClass()) {
-					this.classList.remove(className);
-				} else {
-					this.classList.add(className);
-				}
-			});
+		scope.$on('$destroy', function(){
+			element.unbind();
+		});
+	};
 
-			scope.$on('$destroy', function(){
-				element.unbind();
-			});
-		};
+    var directive = {
+        link: link,
+        restrict: 'A'
+    };
+    return directive;
+}
 
-	    var directive = {
-	        link: link,
-	        restrict: 'A'
-	    };
-	    return directive;
-	}
-
-	angular.module('community.directives')
-		.directive('communityClassToggle', classToggle);
-		
-}(window._));
+angular.module('community.directives')
+	.directive('communityClassToggle', classToggle);
+	
