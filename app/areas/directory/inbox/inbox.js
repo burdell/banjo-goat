6decb2613a217a62ddb6e1bfdb3860798dd07f81
janzheng <janeazy@gmail.com>
2016-02-25 15:31:49 -0500

    - fixed imtefromnow reporting future time; all future time reported as current time. We are not marty mcfly. - inbox messages show unread messages per thread now - inbox message list sticks to the side - inbox messages poll the server /5 secs to keep the thread alive

--- app/areas/directory/inbox/inbox.js
+++ app/areas/directory/inbox/inbox.js
@@ -7,12 +7,17 @@ require('filters/unformattext.js');
 require('filters/timefromnow.js');
 require('filters/wordcut.js');
 
-var inboxController = function($location, $scope, $state, breadcrumbService, inboxService, messageFilter){
+var inboxController = function($location, $scope, $state, breadcrumbService, inboxService, messageFilter, localizationService){
 	var ctrl = this;
+
+	$scope.$on('$stateChangeStart', function(){
+		ctrl.showDetailLoader = true;
+	});
 
 	$scope.$on('$stateChangeSuccess', function(){
 		ctrl.showDetailLoader = false;
 		checkForwardStatus();
+		ctrl.refreshMessages(); // refreshes on new message
 	});
 
 	function checkForwardStatus(){
@@ -24,6 +29,9 @@ var inboxController = function($location, $scope, $state, breadcrumbService, inb
 	function setMessageData(result, updates) {
 		ctrl.messageList = result;
 		checkForwardStatus();
+
+		if (result.totalElements < 1)
+			$state.go('inbox.newtopic');
 	};
 	messageFilter.set({ onFilter: setMessageData });
 	var initialNewMessageCount = inboxService.newDataCount;
@@ -40,6 +48,24 @@ var inboxController = function($location, $scope, $state, breadcrumbService, inb
 		refreshMessages: function(){
 			initialNewMessageCount = inboxService.newDataCount;
 			messageFilter.filter({ page: 1 });
+		},
+		isActive: function(currentId){
+			if (currentId == $state.params.messageId) return true;
+		},
+		newThreadCount: function(){
+			var messageList = ctrl.messageList;
+			var unreadThreads = 0;
+			for(var i=0; i<messageList.content.length; i++) {
+				if (messageList.content[i].unreadCount > 0)
+					unreadThreads++;
+			}
+			return unreadThreads
+		},
+		getSubject: function(subject){
+			if (subject!="") 
+				return subject;
+			else 
+				return "(" + localizationService.data.directory.inbox.noSubject + ")";
 		}
 	});
 };
@@ -47,7 +73,11 @@ inboxController.$inject = [
 	'$location',
 	'$scope', 
 	'$state',
-	require('services/breadcrumb.js'), require('services/inbox.js'),  'InboxMessageFilter'];
+	require('services/breadcrumb.js'),
+	require('services/inbox.js'),
+	'InboxMessageFilter',
+	'CommunityLocalizationService'
+];
 
 angular.module('community.directory')
 	.controller('InboxController', inboxController);
